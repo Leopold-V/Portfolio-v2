79bc8b1c9fd35d9976a6b73f4d4b2ea14c937774
Leopold-V <leopoldV79@gmail.com>
2021-08-10 16:48:33 +0200

    fix typo

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,5 +1,5 @@
 import React, { ReactNode } from 'react'
-import styles from '../styles/Layout.module.scss';
+import styles from '../styles/layout.module.scss';
 
 export const Layout = ({ children }: { children: ReactNode}) => {
     return (
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import styles from '../styles/Navbar.module.scss';
+import styles from '../styles/navbar.module.scss';
 import Link from 'next/link'
 import { TitlePrimary } from './Typography/Title';
 
